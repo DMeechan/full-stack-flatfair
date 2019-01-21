8faefb1902a2637293b533b6ac271a11b9b1a2ab
Daniel Meechan <daniel.meechan.1@gmail.com>
2019-01-21 09:19:46 +0000

    Created simple flatbond component

---
+++ components/Flatbond.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <p>
+        Flatbond for client {{ client_id }}
+        Fee? {{ fixed_membership_fee }}
+        Amount? {{ fixed_membership_fee_amount }}
+    </p>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        client_id: Number,
+        fixed_membership_fee: Boolean,
+        fixed_membership_fee_amount: Number
+    }
+}
+</script>
