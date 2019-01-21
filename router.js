52dbe58ed7459ca033ce901eed267976047c1a42
Daniel Meechan <daniel.meechan.1@gmail.com>
2019-01-21 17:51:39 +0000

    Removed old files

--- router.js
+++
@@ -1,7 +0,0 @@
-// // https://github.com/nuxt-community/router-module
-
-// import Vue from 'vue'
-// import Router from 'vue-router'
-
-// import Create from '~/components/create'
-// import  from '~/components/create'
