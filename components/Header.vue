e6b7818db82d0c12434306ee31128f804dc837ee
Daniel Meechan <daniel.meechan.1@gmail.com>
2019-01-21 09:15:37 +0000

    Created nav bar and added page styling

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-layout-header>
+    <a-menu v-model="currentRoute" mode="horizontal" theme="dark" :style="{ lineHeight: '64px' }">
+      <a-row>
+        <a-col :span="22"/>
+        <a-col :span="2">
+          <a-menu-item key="create">
+            <a-icon type="plus"/>Create a Flatbond
+          </a-menu-item>
+        </a-col>
+      </a-row>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentRoute: ['create']
+    }
+  }
+}
+</script>
