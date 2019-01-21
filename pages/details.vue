9937c6d36bfb2fbf6b14907b648391a89274dc01
Daniel Meechan <daniel.meechan.1@gmail.com>
2019-01-21 16:42:58 +0000

    Added back button to Details page

--- pages/details.vue
+++ pages/details.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-      {{ name }}
-      <Flatbond :flatbond="flatbond"/>
+    <a-row>
+      <a-col :span="22">
+        {{ name }}
+        <Flatbond :flatbond="flatbond"/>
+      </a-col>
+      <a-col :span="2">
+        <nuxt-link :to="`/create`">
+            <a-button type="primary" icon="backward">Go</a-button>
+        </nuxt-link>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
